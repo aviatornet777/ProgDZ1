b35b5a3709409f192fdf4a6bf584b6cfafcdfdef
Serg <Serg.srav@mail.ru>
2023-05-24 11:27:33 +0500

    Задача №1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿          // Домашнее задание
+  // Задача 1
+
+
+  Console.WriteLine("Введите первое число  ");
+  int a=Convert.ToInt32(Console.ReadLine());
+
+  Console.WriteLine("Введите второе число  ");
+  int b=Convert.ToInt32(Console.ReadLine());
+   
+ if(a>b)
+ {
+    Console.Write("Максимальное значение = ");
+    Console.WriteLine(a);
+    Console.Write("Минимальное значение = ");
+    Console.Write(b);
+ }
+ else if(a<b)
+ {
+    Console.Write("Максимальное значение = ");
+    Console.WriteLine(b);
+    Console.Write("Минимальное значение = ");
+    Console.Write(a);
+ }
+ else if(a==b)
+ {
+    Console.WriteLine("Значения равны");
+ }
