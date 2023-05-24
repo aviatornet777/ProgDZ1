571da387b5d7f0a2a69358216f053899495cd914
Serg <Serg.srav@mail.ru>
2023-05-24 11:49:32 +0500

    Задача № 2

--- Program.cs
+++ Program.cs
@@ -1,28 +1,53 @@
 ﻿          // Домашнее задание
-  // Задача 1
+  // Задача №1
 
 
-  Console.WriteLine("Введите первое число  ");
+ // Console.WriteLine("Введите первое число  ");
+  //int a=Convert.ToInt32(Console.ReadLine());
+
+  //Console.WriteLine("Введите второе число  ");
+  //int b=Convert.ToInt32(Console.ReadLine());
+   
+ //if(a>b)
+ //{
+   // Console.Write("Максимальное значение = ");
+    //Console.WriteLine(a);
+    //Console.Write("Минимальное значение = ");
+    //Console.Write(b);
+ //}
+ //else if(a<b)
+ //{
+   // Console.Write("Максимальное значение = ");
+    //Console.WriteLine(b);
+    //Console.Write("Минимальное значение = ");
+    //Console.Write(a);
+ //}
+ //else if(a==b)
+ //{
+   // Console.WriteLine("Значения равны");
+ //}
+
+
+
+
+        //Задача №2
+
+
+ Console.WriteLine("Введите первое число  ");
   int a=Convert.ToInt32(Console.ReadLine());
 
   Console.WriteLine("Введите второе число  ");
   int b=Convert.ToInt32(Console.ReadLine());
-   
- if(a>b)
- {
+
+  Console.WriteLine("Введите третье число  ");
+  int c=Convert.ToInt32(Console.ReadLine());
+  
+  int max=0;
+ if(a>max) max = a;
+ if(b>max) max = b;
+ if(c>max) max = c;
+ 
+    Console.WriteLine(max);
     Console.Write("Максимальное значение = ");
-    Console.WriteLine(a);
-    Console.Write("Минимальное значение = ");
-    Console.Write(b);
- }
- else if(a<b)
- {
-    Console.Write("Максимальное значение = ");
-    Console.WriteLine(b);
-    Console.Write("Минимальное значение = ");
-    Console.Write(a);
- }
- else if(a==b)
- {
-    Console.WriteLine("Значения равны");
- }
+    Console.WriteLine(max);
+ 
